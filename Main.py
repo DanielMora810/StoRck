c42c071d016891dd9adad6ff2288608841341c55
JavierBarreto <javierbarreto1026@gmail.com>
2025-11-24 14:06:09 -0500

    se agrego Cash, al main

--- Main.py
+++ Main.py
@@ -6,6 +6,24 @@ from Login.loginylogout import (
     registrar_usuario
 )
 
+
+# --- Cash ---
+from Cash.Gananciasyperdidas import (
+    cargar_registros,
+    registro_gana,
+    registro_pierde,
+    mostrar_resumen,
+    filtro_rangofecha
+)
+
+# cargar registros de cash al iniciar
+cargar_registros()
+
+
+# ==============================
+#      MENÚ PRINCIPAL
+# ==============================
+
 def menu_principal():
     while True:
         print("\n===== MENÚ PRINCIPAL =====")
@@ -13,11 +31,16 @@ def menu_principal():
         print("2. Cerrar sesión")
         print("3. Estado de sesión")
         print("4. Cambiar contraseña")
-        print("5. Registrar nuevo usuario")
-        print("6. Salir")
+        print("5. Registrar usuario nuevo")
+        print("6. Registrar Ganancias")
+        print("7. Registrar Perdidas")
+        print("8. Mostrar resumen del día")
+        print("9. Filtrar por rango de fechas")
+        print("10. Salir")
 
         opcion = input("Seleccione una opción: ")
 
+        # ------ LOGIN ------
         if opcion == "1":
             usuario = input("Usuario: ")
             contraseña = input("Contraseña: ")
@@ -35,7 +58,21 @@ def menu_principal():
         elif opcion == "5":
             print(registrar_usuario())
 
+        # ------ CASH ------
         elif opcion == "6":
+            registro_gana()
+
+        elif opcion == "7":
+            registro_pierde()
+
+        elif opcion == "8":
+            mostrar_resumen()
+
+        elif opcion == "9":
+            filtro_rangofecha()
+
+        # ------ SALIDA ------
+        elif opcion == "10":
             print("Saliendo del sistema...")
             break
 
@@ -43,5 +80,5 @@ def menu_principal():
             print("Opción inválida, intente de nuevo.")
 
 
-# Ejecutar menú
+# Iniciar programa
 menu_principal()
